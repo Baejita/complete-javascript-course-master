08f6070b503df4f7ab2d80321b5579e08077f169
Baejita <homokoro@gmail.com>
2024-02-11 19:57:06 +0700

    scrolling 1

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -42,29 +42,29 @@ document.addEventListener('keydown', function (e) {
 /////////////////////////////////////////////
 /////////////////////////////////////////////
 
-//**-- Selected Element */
-console.log(document.documentElement);
-console.log(document.head);
+// //**-- Selected Element */
+// console.log(document.documentElement);
+// console.log(document.head);
 
-const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section')
-console.log(allSections);
+// const header = document.querySelector('.header');
+// const allSections = document.querySelectorAll('.section')
+// console.log(allSections);
 
-document.getElementById('section-1');
-const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
+// document.getElementById('section-1');
+// const allButtons = document.getElementsByTagName('button');
+// console.log(allButtons);
 
-//ถ้าเป็น NODELIST ลบจะไม่หายไปจาก หน้าจอ
+// //ถ้าเป็น NODELIST ลบจะไม่หายไปจาก หน้าจอ
 
-console.log(document.getElementsByClassName('btn'))
+// console.log(document.getElementsByClassName('btn'))
 
 
-//***---- Creating and inserting elements--- *** */
-//.insertAdjacentHTML
+// //***---- Creating and inserting elements--- *** */
+// //.insertAdjacentHTML
 
-const message = document.createElement('div');
-message.classList.add('cookie-message')
-message.innerHTML = 'We use cookie for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>'
+// const message = document.createElement('div');
+// message.classList.add('cookie-message')
+// message.innerHTML = 'We use cookie for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>'
 
 // แทรกเข้าไปใน dom
 // header.prepend(message); //อยู่ด้านบนสุดของกล่อง --ใน element ของ header
@@ -73,53 +73,75 @@ message.innerHTML = 'We use cookie for improved functionality and analytics. <bu
 //ถ้้าอยากมีทั้งบนและล่าง ให้ใช้ clone
 //header.append(message.cloneNode(true));
 
-header.before(message); //ไปสร้างกล่องอยู่ --- ก่อน header
+// header.before(message); //ไปสร้างกล่องอยู่ --- ก่อน header
 
 
-//---- DELETE elements --------------------------------/
-  document.querySelector('.btn--close-cookie').addEventListener('click', function(){
-    message.remove()
-  })
+// //---- DELETE elements --------------------------------/
+//   document.querySelector('.btn--close-cookie').addEventListener('click', function(){
+//     message.remove()
+//   })
 
-  //styles เราไม่สามารถ log อออกค่า height ได้แต่เราใช้คำสั่งนี้ได้ 
+//   //styles เราไม่สามารถ log อออกค่า height ได้แต่เราใช้คำสั่งนี้ได้ 
 
-  console.log(getComputedStyle(message).height); //47.8261px
-  //เปลี่ยน style ได้
-  message.style.backgroundColor = '#37383d'
-//เพิ่มความสูง
-  message.style.height = Number.parseFloat(getComputedStyle(message).height,10) + 40+'px' ; //ต้องแปลงให้เป็น 
+//   console.log(getComputedStyle(message).height); //47.8261px
+//   //เปลี่ยน style ได้
+//   message.style.backgroundColor = '#37383d'
+// //เพิ่มความสูง
+//   message.style.height = Number.parseFloat(getComputedStyle(message).height,10) + 40+'px' ; //ต้องแปลงให้เป็น 
 
-// การเปลี่ยนคุณสมบัตรผ่าน CSS setPoroperty ('ชื่อของสื่งที่จะเปลี่ยน)
-  document.documentElement.style.setProperty('--color-primary', 'orangered');
+// // การเปลี่ยนคุณสมบัตรผ่าน CSS setPoroperty ('ชื่อของสื่งที่จะเปลี่ยน)
+//   document.documentElement.style.setProperty('--color-primary', 'orangered');
 
-  //Attributes standard
-  const logo = document.querySelector('.nav__logo');
-  console.log(logo.alt);
-  console.log(logo.src);
-  console.log(logo.className);
+//   //Attributes standard
+//   const logo = document.querySelector('.nav__logo');
+//   console.log(logo.alt);
+//   console.log(logo.src);
+//   console.log(logo.className);
 
-  logo.alt = 'Beautiful mininalist logo';// เปลี่ยน ชื่อ attibute
+//   logo.alt = 'Beautiful mininalist logo';// เปลี่ยน ชื่อ attibute
 
-  //Non-standard
-  console.log(logo.designer); // แบบนี้ถึงจะได้
-  console.log(logo.getAttribute('designer'));  // แบบนี้ถึงจะได้
-  logo.setAttribute('company', 'Bankist') // เพิ่ม **** 
+//   //Non-standard
+//   console.log(logo.designer); // แบบนี้ถึงจะได้
+//   console.log(logo.getAttribute('designer'));  // แบบนี้ถึงจะได้
+//   logo.setAttribute('company', 'Bankist') // เพิ่ม **** 
 
-  console.log(logo.src); //http://127.0.0.1:8080/img/logo.png
-  console.log(logo.getAttribute('src')); //img/logo.png
+//   console.log(logo.src); //http://127.0.0.1:8080/img/logo.png
+//   console.log(logo.getAttribute('src')); //img/logo.png
 
-  const link = document.querySelector('.nav__link--btn')
-  console.log(link.href);
-  console.log(link.getAttribute('href'));
+//   const link = document.querySelector('.nav__link--btn')
+//   console.log(link.href);
+//   console.log(link.getAttribute('href'));
 
-  ////****** ถ้าราตั้งชื่อ attribute เป็นแบบขีด เช่น data-version-number เราต้องเลือก */
-  console.log(logo.dataset.versionNumber);
+//   ////****** ถ้าราตั้งชื่อ attribute เป็นแบบขีด เช่น data-version-number เราต้องเลือก */
+//   console.log(logo.dataset.versionNumber);
 
-  //Classes แนะนำให้ใช้วิธีนี้
-logo.classList.add('c','j')
-logo.classList.remove('c','j')
-logo.classList.toggle('c','j')
-logo.classList.contains('c','j') //not includes
+//   //Classes แนะนำให้ใช้วิธีนี้
+// logo.classList.add('c','j')
+// logo.classList.remove('c','j')
+// logo.classList.toggle('c','j')
+// logo.classList.contains('c','j') //not includes
 
-//Don't USSE 
-logo.className = 'Jonas'
+// //Don't USSE 
+// logo.className = 'Jonas'
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+//หาตำแหน่งของ section1 เมื่อคลิก
+btnScrollTo.addEventListener('click',function(e) {
+  const s1coods = section1.getBoundingClientRect();
+  console.log(s1coods);
+  console.log(e.target.getBoundingClientRect());//หาตำแหน่งของตัวปุ่น btnScollto = Learn more
+  console.log('Current scroll (x/y', window, pageXOffset,pageYOffset); //กดตรงปุ่น้เพื่อดูว่าหน้าจอเลื่นลงมาเท่าไหร่แล้ว
+
+  console.log('Height/width viewport', document.documentElement.clientHeight, document.documentElement.clientWidth); //จะทราบว่าความกว้าง x สูง ของหน้าจอเท่าไหร่
+
+  //Scrolling
+  window.scrollTo(s1coods
+    .left 
+    + window.pageXOffset, s1coods.top + window.pageYOffset
+    )
+
+
+
+})
