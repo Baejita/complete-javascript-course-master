35bb513b022fef0604ddd4ea98e11af7cde379f1
Baejita <homokoro@gmail.com>
2024-02-12 15:27:52 +0700

    propagation1

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -124,24 +124,88 @@ document.addEventListener('keydown', function (e) {
 // //Don't USSE 
 // logo.className = 'Jonas'
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+// const btnScrollTo = document.querySelector('.btn--scroll-to');
+// const section1 = document.querySelector('#section--1');
 
-//หาตำแหน่งของ section1 เมื่อคลิก
-btnScrollTo.addEventListener('click',function(e) {
-  const s1coods = section1.getBoundingClientRect();
-  console.log(s1coods);
-  console.log(e.target.getBoundingClientRect());//หาตำแหน่งของตัวปุ่น btnScollto = Learn more
-  console.log('Current scroll (x/y', window, pageXOffset,pageYOffset); //กดตรงปุ่น้เพื่อดูว่าหน้าจอเลื่นลงมาเท่าไหร่แล้ว
+// //หาตำแหน่งของ section1 เมื่อคลิก
+// btnScrollTo.addEventListener('click',function(e) {
+//   const s1coods = section1.getBoundingClientRect();
+//   console.log(s1coods);
+//   console.log(e.target.getBoundingClientRect());//หาตำแหน่งของตัวปุ่น btnScollto = Learn more
+//   console.log('Current scroll (x/y', window, pageXOffset,pageYOffset); //กดตรงปุ่น้เพื่อดูว่าหน้าจอเลื่นลงมาเท่าไหร่แล้ว
 
-  console.log('Height/width viewport', document.documentElement.clientHeight, document.documentElement.clientWidth); //จะทราบว่าความกว้าง x สูง ของหน้าจอเท่าไหร่
+//   console.log('Height/width viewport', document.documentElement.clientHeight, document.documentElement.clientWidth); //จะทราบว่าความกว้าง x สูง ของหน้าจอเท่าไหร่
 
-  //Scrolling
-  window.scrollTo(s1coods
-    .left 
-    + window.pageXOffset, s1coods.top + window.pageYOffset
-    )
+//   //Scrolling
+//   // window.scrollTo(s1coods
+//   //   .left 
+//   //   + window.pageXOffset, s1coods.top + window.pageYOffset
+//   //   )
+
+//   //วิธีแบบเก่า
+//     // window.scrollTo({
+//     //   left: s1coods.left + window.pageXOffset,
+//     //   top: s1coods.top + window.pageYOffset,
+//     //   behavior: 'smooth'
+//     // });
+
+//     //วิธีแบบใหม่
+//     section1.scrollIntoView({
+//       behavior: 'smooth'
+//     })
+
+// })
+
+//Lecture addEventListener
+
+// const h1 = document.querySelector('h1');
+// const alertH1 =  function(e){ alert('addEvenlistener: Great! You are reading the heading :D')
+
+// h1.removeEventListener('mouseenter',alertH1) // ทำให้เราฟังฟังก์ชั่นได้เพียงคั้งเดียวเท่านั้น 
+
+// }
 
 
+// //ปัจจุบันนิยมใช้แบบนี้มากกว่า เพราะสามารถเพิ่มหลายตัวได้ และสามารถใส่ตัวแปรเพิ่มได้เรื่อย ๆ 
+// h1.addEventListener('mouseenter',alertH1)
 
+// setTimeout(() =>h1.removeEventListener('mouseenter',alertH1) , 3000)
+// // //อันนี้แบบเก่า
+// // h1.onmouseenter = function(e){
+// //   alert('onmouseenter: Great! You are reading the heading :D')
+// // }
+
+
+////////////////////*****/------------------------ */
+
+//rgb(255,255,255)
+/// สร้างตัวแปรที่เก็บ ฟังก์ชั่น การสร้างการสุ่ม rgb (ตัวเลข ก่อน)
+
+const randomInt = (min,max) => Math.floor(Math.random()* max - min +1);
+const randomColor = () => `rgb(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)})`
+console.log(randomColor());
+
+
+//// แล้วค่อยมาใส่ฟังก์ชั่นเมื่อกด link ให้เปลี่ยนสีตามตัวแปร randomColor
+//ถ้าคลิก element ย่อยสุดจะทำให้ parent เปลี่ยนสีตามไปด้วยเสมอ แต่ถ้า คลิกอันพ่อแม่ใหญ่กว่า เด็กๆ จะไม่เปลี่ยนตาม 
+document.querySelector('.nav__link').addEventListener('click', function(e){
+ this.style.backgroundColor = randomColor()
+ console.log('LINK', e.target ,e.currentTarget); //เมื่อคลิกแล้วจะหาเป้าหมายว่ากดตรงไหน
+ console.log(e.currentTarget === this);
+ 
+ //หยุดการส่งต่อการเกิดเหตุการ์ณ์****----------*****
+ //e.stopPropagation() // โดยทั่วไปไม่ควรหยุดการ propagation 
 })
+
+document.querySelector('.nav__links').addEventListener('click', function(e){
+  this.style.backgroundColor = randomColor()
+  console.log('CONTAINER', e.target ,e.currentTarget);
+  console.log(e.currentTarget === this);
+})
+
+document.querySelector('.nav').addEventListener('click', function(e){
+  this.style.backgroundColor = randomColor()
+  console.log('NAVBAR', e.target,e.currentTarget);
+  console.log(e.currentTarget === this);
+},
+true) //ถ้าใช่ true จะทำให้เมื่อเราคลิก nav จะส่งผลแต่ nav ไม่ผ่านตัวเอื่น ๆเข้าไป
