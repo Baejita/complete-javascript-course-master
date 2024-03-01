a76e9ab83edd06ef34f27e16d01070cd6b00c99e
Baejita <homokoro@gmail.com>
2024-03-01 20:25:39 +0700

    form submit

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,51 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if(navigator.geolocation) 
+navigator.geolocation.getCurrentPosition(function(position){
+
+    const {latitude} = position.coords;
+    const {longitude} = position.coords;
+    console.log(`https://www.google.co.th/maps/@${latitude},-${longitude}`);
+
+    const coords = [latitude-0.045, longitude+0.61];
+    console.log(coords);
+
+    const map = L.map('map').setView(coords, 13);
+
+L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+
+    map.on('click', function (mapEvent) {
+       form.classList.remove('hidden');
+       inputDistance.focus();
+       
+       // console.log(mapEvent);
+        
+
+        
+    //     const {lat, lng} = mapEvent.latlng; 
+   
+    // L.marker([lat, lng]).addTo(map).bindPopup(L.popup({
+    //     maxwidth: 250,
+    //     minwidth: 100,
+    //     autoClose: false,
+    //     closeOnClick: false,
+    //     className: 'running-popup',
+
+    // }))
+    // .setPopupContent('Workout')
+    // .openPopup();
+})
+
+}, function(){
+    alert('Could not get your position')
+});
+
+form.addEventListener('submit', function(){
+    
+})
