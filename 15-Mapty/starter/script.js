8d53ba9d517d13df56d13d47b7b5b388634901db
Baejita <homokoro@gmail.com>
2024-03-02 17:32:03 +0700

    check valid

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -3,6 +3,58 @@
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+class Workout {
+    date = new Date();
+    id = (Date.now()+ '').slice(-10);
+
+
+    constructor(coords, distance, duration){
+        // this.date = ...
+        // this.id = ...
+        this.coords = coords; // [lat, lng]
+        this.distance = distance; // in km
+        this.duration = duration; // min
+    }
+}
+
+class Running extends Workout {
+    constructor(coords, distance, duration, cadence){
+        super(coords, distance, duration);
+        this.cadence = cadence;
+        this.calcPace();
+    }
+
+    calcPace(){
+        //min / km
+        this.pace = this.duration / this.distance;
+        return this.space;
+    }
+ }
+
+class Cycling extends Workout {
+    constructor(coords, distance, duration, elevationGain){
+        super(coords, distance, duration);
+        this.elevationGain = elevationGain;
+        this.calcSpeed();
+    }
+
+    calcSpeed() {
+        // km/h
+        this.speed = this.distance / (this.duration/60);
+        return this.speed;
+    }
+ }
+
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycling1 = new Cycling([39, -12], 27, 98, 523);
+
+// console.log(run1,cycling1);
+
+
+
+ /////////////////////
+ //Application Architecture
+
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
@@ -10,9 +62,7 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-
-
-class App {
+ class App {
     #map;
     #mapEvent;
 
@@ -63,11 +113,42 @@ class App {
     }
 
     _newWorkout(e){
+            const validInputs = (...inputs) => inputs.every(inp => Number.isFinite(inp));
+
+
         e.preventDefault();
-             
-            //clear input distance
-            inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value ='';
-    
+
+            //get data from form;
+            const type = inputType.value;
+            const distance = +inputDistance.value;
+            const duration = +inputDuration.value;
+            
+            
+
+            //if  workout running, create runing object
+            
+            if(type ==='running'){
+                //check if data is valid
+                const cadence = +inputCadence.value;
+                if(
+                // !Number.isFinite(distance) ||
+                // !Number.isFinite(duration)  ||
+                // !Number.isFinite(cadence) 
+                    !validInputs(distance, duration, cadence)
+                ) 
+                return alert('Input have to be positive number');
+            }
+
+            //if workout cycling, create cylcing object
+            if(type ==='cycling'){
+                const elevation = +inputElevation.value;
+                if(!validInputs(distance, duration, elevation)
+                    ) 
+                    return alert('Input have to be positive number');
+            }
+            //Add new object ot workout array
+
+            //render workout on map as marker
             const {lat, lng} = this.#mapEvent.latlng; 
            
             L.marker([lat, lng]).addTo(this.#map).bindPopup(L.popup({
@@ -80,6 +161,13 @@ class App {
             }))
             .setPopupContent('Workout')
             .openPopup();
+            //render workout on the list
+            
+
+            //hide form and clear input fields
+            inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value ='';
+    
+            
     }
 }
 
