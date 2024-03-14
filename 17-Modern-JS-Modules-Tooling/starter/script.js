06c1c807e002514a23b6950521c97ec499619eaa
Baejita <homokoro@gmail.com>
2024-03-14 23:19:21 +0700

    555

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -9,6 +9,84 @@ console.log('importing modules');
 // shopiingCart.addToCart('bread', 5);
 // console.log(shopiingCart.totalPrice);
 
-import add, {addToCart , totalPrice as price, tq}  from './shopiingCart.js';
+// import add, {addToCart , totalPrice as price, tq}  from './shopiingCart.js';
+// add('pizz', 6);
+// console.log(price);
+
+
+import add, {cart} from './shopiingCart.js';
 add('pizz', 6);
-console.log(price);
+add('bread', 2);
+add('butter', 1);
+console.log(cart);
+
+/*
+const getLastPost = async function() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await res.json();
+    
+    return { title : data.at(-1).title, text : data.at(-1).body };
+
+}
+
+const lastPort = getLastPost()
+console.log(lastPort);
+
+
+//not Clean
+// lastPort.then( last  => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
+
+*/
+
+/*
+const ShoppingCart2 = (function() {
+    const cart = [];
+    const shippingCost = 10;
+    const totalPrice = 256;
+    const totalQuantity = 56;
+
+   const addToCart = function (product, quantity){
+        cart.push({product, quantity});
+        console.log(`${quantity} ${product} added to cart`);
+   };
+    const orderStock = function (product, quantity){
+            console.log(`${quantity} ${product} ordered from supplier`);
+    };
+
+    return {
+        addToCart,
+        cart,
+        totalPrice,
+        totalQuantity
+    }
+}) ()
+
+ShoppingCart2.addToCart('apple', 5)
+ShoppingCart2.addToCart('pizza', 2)
+console.log(ShoppingCart2);
+
+*/
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+
+const state = {
+    cart: [
+        {product : 'Pizza', quantity: 3},
+        {product : 'Bread', quantity: 5},
+    ],
+    user: { loggedIn: true}
+}
+
+//วิธีปกติแบบไม่ใช้ module
+const stateclone = Object.assign({state});
+
+//วิธีใช้ module ในการโคลน
+const statCloneSet = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateclone); 
+console.log(statCloneSet);
--- 17-Modern-JS-Modules-Tooling/starter/shopiingCart.js
+++ 17-Modern-JS-Modules-Tooling/starter/shopiingCart.js
@@ -2,7 +2,7 @@
 console.log('Exporting modules');
 
 const shippingCost = 10;
-const cart = [];
+export const cart = [];
 
 export const addToCart = function (product, quantity){
     cart.push({product, quantity});
@@ -13,8 +13,16 @@ export {totalPrice, totalQuantity as tq}
 const totalPrice = 1020;
 const totalQuantity = 99;
 
+//สร้างฟังชั่นส่งออกที่ทำให้เข้าไปเลย
 export default  function (product, quantity){
     cart.push({product, quantity});
     console.log(`${quantity} ${product} added to cart`);
 }
+
+
+//blocking code 
+// console.log('start fetching users');
+// await fetch('https://jsonplaceholder.typicode.com/users')
+// console.log('Finish fetching users');
+
 
